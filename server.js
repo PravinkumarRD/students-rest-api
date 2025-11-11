0a2ec70cf98836742fd1ea57200af73c2f30a24a
PravinkumarRD <dabade.pravinkumar@gmail.com>
2025-11-11 22:16:10 +0530

    Addition of New Middleware

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const morgan = require("morgan");
 const cors = require("cors");
 
-const { authorizationMiddleware } = require("./src/middleware/authorization-middleware");
+const {
+  authorizationMiddleware,
+} = require("./src/middleware/authorization-middleware");
 const userRoutes = require("./src/routes/user-routes");
 const eventRoutes = require("./src/routes/event-routes");
 const employeeRoutes = require("./src/routes/employee-routes");
@@ -11,12 +13,22 @@ const employeeRoutes = require("./src/routes/employee-routes");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-
-app.use(cors({
-  origin: ['http://localhost:4200', 'capacitor://localhost', 'file://'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+app.use(
+  cors({
+    origin: ["http://localhost:4200", "capacitor://localhost", "file://"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,4 +38,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/events", authorizationMiddleware, eventRoutes);
 app.use("/api/employees", authorizationMiddleware, employeeRoutes);
 
-app.listen(PORT, () => console.log(`Students Events Service is running on PORT ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Students Events Service is running on PORT ${PORT}`)
+);
