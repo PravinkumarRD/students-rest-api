3c9d88f04821e11d78598ca9d79cc6982d3ff4f7
PravinkumarRD <dabade.pravinkumar@gmail.com>
2025-11-11 15:36:14 +0530

    Removed Company Specific Headers

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization,bajaj-authorization-token"
+    "Content-Type, Authorization"
   );
   next();
 });
