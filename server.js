044bb6641d7cb02fc90677aab8b6c75144ee9e2e
PravinkumarRD <dabade.pravinkumar@gmail.com>
2025-11-11 19:30:06 +0530

    CORS Changes Revision

--- server.js
+++ server.js
@@ -14,22 +14,21 @@ const employeeRoutes = require("./src/routes/employee-routes");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  next();
+});
 app.use(cors({
-  origin: "*",
+  origin: "*", // or "https://yourdomain.com"
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: false
 }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
